aef00a07a830233804269eccf99baf9e8b0f9ffc
Henry <emorell96@hotmail.com>
2022-04-10 10:26:52 +0200

    Add project files.

---
+++ SnackbarBug/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SnackbarBug;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ SnackbarBug/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace SnackbarBug;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		CounterLabel.Text = $"Current count: {count}";
+		var snackbar = new Snackbar
+		{
+			Action = () => Console.WriteLine("Test"),
+			ActionButtonText = "Test button",
+			Text = "Test Text",
+
+		}; 
+		snackbar.Show();
+		
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+}
+
